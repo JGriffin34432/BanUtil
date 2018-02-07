c48f616a1b837add090cd451b9f44078de78c749
Joshua Griffin <JoshTheGamer632@outlook.com>
2018-02-07 11:02:21 -0500

    Update BanUtil.java
    Changed playing to "playing... DOJRP | Grand Theft Auto V".

--- src/main/java/xyz/sidetrip/banutil/BanUtil.java
+++ src/main/java/xyz/sidetrip/banutil/BanUtil.java
@@ -105,7 +105,7 @@ public class BanUtil implements Runnable {
     @EventSubscriber
     public void onLoginEvent(ShardReadyEvent event) throws InterruptedException{
         IDiscordClient client = event.getClient();
-        client.changePlayingText("Fortnite");
+        client.changePlayingText("DOJRP | Grand Theft Auto V");
         checkBot();
     }
 
